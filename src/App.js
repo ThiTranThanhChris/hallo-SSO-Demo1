675c97399e5264b49626edec9e4f829d1927d82e
ThiTranThanhChris <88011956+ThiTranThanhChris@users.noreply.github.com>
2024-12-11 09:25:45 +0700

    Remove code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-import { useState } from 'react';
+
 import './App.scss';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from 'containers/Login/Login.js';
-import Preferences from 'containers/Preferences/Preferences';
-import Dashboard from 'containers/Dashboard/Dashboard';
+
 
 function App() {
-  const [token, setToken] = useState();
-
-  if (!token) {
-    return <Login setToken={setToken} />;
-  }
-
   return (
     <div>
       <h1>Application</h1>
--- src/components/Button/Button.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Button = ({ type = "button", label = "" }) => {
-  return <button type={type}>{label}</button>;
-};
-
-export default Button;
